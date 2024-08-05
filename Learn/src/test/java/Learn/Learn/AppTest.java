38d53146ddec40f36bafda77a4d0591727bf06fc
Vinayak <Vinayak@Vinayak-Dev>
2024-08-05 11:50:13 +0530

    First push

---
+++ Learn/src/test/java/Learn/Learn/AppTest.java
@@ -0,0 +1,20 @@
+package Learn.Learn;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
