9f42f6b6666bbf1c6354d851f4b519deda699442
Vinayak <Vinayak@192.168.9.92>
2024-09-18 12:04:07 +0530

    EOD

---
+++ Learn/src/main/java/LearnJava/checkEvenOdd.java
@@ -0,0 +1,23 @@
+package LearnJava;
+
+import java.util.Scanner;
+
+public class checkEvenOdd {
+
+	public static void main(String[] args) {
+		int num = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(" enter the number to be checked as odd or even : ");
+		num = sc.nextInt();
+		sc.close();
+		System.out.println("entered number is :" + num);
+
+		if (num % 2 == 0)
+			System.out.println("number is even");
+		else
+			System.out.println("Number is odd");
+	}
+
+}
